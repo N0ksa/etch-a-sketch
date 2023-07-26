0f96a23103179161976c36f1c10844d790f51811
Leon <leonsarko7@gmail.com>
2023-07-26 09:34:48 +0200

    Add drawCanvas() and deleteCanvas()
    Added functions to draw and delete canvas. Made a button eventListener for clear, which calls these functions

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 const canvas = document.querySelector(".container");
+const clearButton = document.getElementById("clear");
 
-for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
+
+let sizeOfCanvas = 16;
+let pixelColor = "black";
+
+drawCanvas();
+
+
+
+clearButton.addEventListener("click", () => {
+    console.log();
+    deleteCanvas();
+    drawCanvas();
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+function drawCanvas(){
+    for (let i = 0; i < sizeOfCanvas * sizeOfCanvas; i++){
         const pixel = document.createElement("div");
         pixel.classList.add("pixel");
-        pixel.style.width = 500/16 + "px";
-        pixel.style.height = 500/16 + "px";
+        pixel.style.width = 500/sizeOfCanvas + "px";
+        pixel.style.height = 500/sizeOfCanvas + "px";
         canvas.appendChild(pixel);
     }
+
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => {
+    pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = pixelColor;
+        })
+    })
+
 }
 
-const pixels = document.querySelectorAll(".pixel");
-pixels.forEach(pixel => {
-    pixel.addEventListener("mouseover", () =>{
-        pixel.style.backgroundColor = "black";
-    })
-})
+
+function deleteCanvas(){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.lastChild);
+    }
+}
